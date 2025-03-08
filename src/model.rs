749da5ef3e9022d3b1b8e605d0745ed234502bbc
kenny <kenny@based.win>
2025-03-07 22:49:42 -0500

    Fix JsonValue import in model.rs
    Signed-off-by: kenny <kenny@based.win>

--- src/model.rs
+++ src/model.rs
@@ -30,7 +30,8 @@ pub struct ProductCategories {
     pub display_type: String,
     pub thumbnail: String,
     pub count: i32, // Count of products in this category
-    //pub child_categories: Option<Vec<Uuid>>, // Array to store child category IDs
+    //pub child_categories: Option<Vec<Uuid>>, // Array of UUIDs for child categories
+    pub child_categories: Option<Vec<JsonValue>>, // JSONB array of child category info
     //TODO turn this into a vector of string arrays Vec<[T; N]>
     //pub terms: Vec<[String; 3]>, // New field: vector of arrays, each with 3 strings
     #[serde(rename = "createdAt")]
